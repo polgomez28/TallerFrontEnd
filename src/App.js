2018f66ad23cfeb9013ff48594c7831670fd45e9
polg <polg28@gmail.com>
2021-07-24 10:11:45 -0300

    texto2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nuevo proyecto React <code>src/App.js</code>
+          Obligatorio React <code>src/App.js</code>
         </p>
         <a
           className="App-link"
