d052db0c6701802cbae128046015fa9b7df4486f
unknown <nadia.olivera.alzugaray@gmail.com>
2021-08-03 17:40:35 -0300

    030820210308202103082021030820210308202103082021030820210308202103082021030820210308202103082021030820210308202103082021030820210308202103082021030820210308202103082021030820210308202103082021030820210308202103082021030820210308202103082021

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+//import { Router } from 'workbox-routing';
+import Home from './componentes/Home';
+import About from './componentes/About';
+import User from './componentes/User';
+import Login from './componentes/Login';
+import NotFound from './componentes/NotFound';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Obligatorio React <code>src/App.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/About" component={About}></Route>
+        <Route path="/User" component={User}></Route>
+        <Route path="/Login" component={Login}></Route>
+        <Route component={NotFound}></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/componentes/Login.js
@@ -0,0 +1,12 @@
+const Login = () => {
+    return (
+        <form>
+            <h2>Login </h2>
+            <input type="text" placeholder="Usuario"></input>
+            <input type="text" placeholder="Contraseña"></input>
+            <button>Ingresar</button>
+        </form>
+    )
+}
+
+export default Login;
