7fc925be2628f49e5700b3cec386de90318398c9
polg <polg28@gmail.com>
2021-08-03 20:19:35 -0300

    modifica texto

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Obligatorio React FrontEnd <code>src/App.js</code>
+          Obligatorio React<code>src/App.js</code>
         </p>
         <a
           className="App-link"
