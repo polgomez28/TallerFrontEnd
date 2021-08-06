02fefbffe5e82a59889a091c8e9b028e929212e4
unknown <nadia.olivera.alzugaray@gmail.com>
2021-08-06 18:06:25 -0300

    mostrar ventas 06.08

--- src/componentes/Dashboard.js
+++ src/componentes/Dashboard.js
@@ -1,7 +1,8 @@
 import { useHistory } from "react-router-dom";
 import { useRef, useState } from "react";
-import { useDispatch} from "react-redux";
-import { Alert, Button, Form } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Alert, Button, Card, Form, ListGroup, ListGroupItem } from "react-bootstrap";
+import { useEffect } from "react";
 
 const Dashboard = () => {
 
@@ -20,14 +21,14 @@ const Dashboard = () => {
     const comprar = async () => {
         console.log("presionando Realizar compra")
 
-       // const token = useSelector((state) => state.authReducer)
+        // const token = useSelector((state) => state.authReducer)
 
         const nombreCliente = clienteRef.current.value;
         const idPaquete = paqueteRef.current.value;
         const cantidadMayores = adultosRef.current.value;
         const cantidadMenores = menoresRef.current.value;
 
-        if(nombreCliente.length === 0 || idPaquete.length === 0 || cantidadMayores.length === 0 || cantidadMenores.length === 0 ){
+        if (nombreCliente.length === 0 || idPaquete.length === 0 || cantidadMayores.length === 0 || cantidadMenores.length === 0) {
             setError('Debe completar todos los campos.');
             return;
         }
@@ -53,15 +54,29 @@ const Dashboard = () => {
 
         const resultado = await response.json();
 
-        if(resultado.token){
+        if (resultado.token) {
             console.log("--------->", resultado);
 
-            dispatch({type: 'AGREGAR_VENTA', payload: resultado.token});
+            dispatch({ type: 'AGREGAR_VENTA', payload: resultado.token });
             setError('');
         }
-        else{
+        else {
             setError(resultado.mensaje);
         }
+
+    }
+
+    const paquetes = useSelector((state) => state.ventasReducer);
+    useEffect(() => {
+        cargarPaquetes();
+    }, []);
+
+    const cargarPaquetes = async () => {
+        const response = await fetch("https://destinos.develotion.com///ventas.php?idVendedor=4")
+
+        const ventas = await response.json();
+
+        dispatch({ type: 'CARGAR_VENTAS', payload: ventas.data })
     }
 
     return (<div className="dashboard">
@@ -70,7 +85,7 @@ const Dashboard = () => {
             <h2>Venta de Paquetes</h2>
             <Form>
                 <Form.Group >
-                    <Form.Control className="input" type="text" placeholder="Nombre Cliente" ref={clienteRef} required/>
+                    <Form.Control className="input" type="text" placeholder="Nombre Cliente" ref={clienteRef} required />
 
                     <Form.Select className="select" ref={paqueteRef}>
                         <option value="1">One</option>
@@ -88,12 +103,44 @@ const Dashboard = () => {
                     Realizar Compra
                 </Button>
 
-                 {error && <Alert variant="danger">{error}</Alert>} 
+                {error && <Alert variant="danger">{error}</Alert>}
 
             </Form>
         </section>
         <section>
             <h2>Listado de Paquetes</h2>
+
+            <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Paquete</Card.Title>
+                        <ListGroup className="list-group-flush">
+                            <ListGroupItem> Cliente: </ListGroupItem>
+                            <ListGroupItem>  Adultos</ListGroupItem>
+                            <ListGroupItem>  Niños</ListGroupItem>
+                            <ListGroupItem> Precio Final: </ListGroupItem>
+                        </ListGroup>
+                    </Card.Body>
+                </Card>
+
+            {/*             
+            {paquetes.map((ventas) =>
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>{ventas.idPaquete}</Card.Title>
+                        <ListGroup className="list-group-flush">
+                            <ListGroupItem> Cliente: {ventas.nombreCliente}</ListGroupItem>
+                            <ListGroupItem> {ventas.cantidadMayores} Adultos</ListGroupItem>
+                            <ListGroupItem> {ventas.cantidadMenores} Niños</ListGroupItem>
+                            <ListGroupItem> Precio Final: </ListGroupItem>
+                        </ListGroup>
+                    </Card.Body>
+                </Card>
+
+            )}; */}
+
         </section>
         <section>
             <h2>Cantidad de Ventas</h2>
@@ -108,7 +155,7 @@ const Dashboard = () => {
 
             <h2>Destinos </h2>
             <h3>Destinos Top</h3>
-            <h3>Destinos a Promocionar</h3> 
+            <h3>Destinos a Promocionar</h3>
 
 
         </section>
