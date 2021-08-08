626b6dcc5ac025eaa31334e7062b5a9ad17b9537
unknown <nadia.olivera.alzugaray@gmail.com>
2021-08-08 19:39:06 -0300

    movidos grafica y ventas por usuario

--- src/componentes/Dashboard.js
+++ src/componentes/Dashboard.js
@@ -9,14 +9,13 @@ import Formulario from "./Formulario";
 const Dashboard = () => {
     const history = useHistory();
     const token = useSelector((state) => state.loginReducer);
-    /*defino destinos*/
 
     /*defino destinos*/
     const destinos = useSelector((state) => state.paquetesReducer)
     const dispatch = useDispatch();
     const [error, setError] = useState('');
     const paquetes = useSelector((state) => state.ventasReducer);
-   
+
     const cargarPaquetes = async () => {
 
         const response = await fetch('https://destinos.develotion.com/paquetes.php', {
@@ -44,79 +43,115 @@ const Dashboard = () => {
     const ventas = useSelector((state) => state.ventasReducer);
 
     useEffect(() => {
-      cargarPaquetes();
-    //   cargarVentas();
+        cargarPaquetes();
+        //   cargarVentas();
     }, []);
-  
+
     const cargarVentas = async () => {
 
-      
-      const response = await fetch('https://destinos.develotion.com/ventas.php?idVendedor={ventas.vendedor_id}')
-      const datos = await response.json();
-  
-      dispatch({ type: 'AGREGAR_VENTA', payload: datos.data });
+        const response = await fetch('https://destinos.develotion.com/ventas.php?idVendedor={ventas.vendedor_id}')
+        const datos = await response.json();
+
+        dispatch({ type: 'AGREGAR_VENTA', payload: datos.data });
+
+        return (
+            /* 3.3 */
+            <section>
+                <h2>Cantidad de Ventas</h2>
+
+                <Table hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>Id Paquete</th>
+                            <th>Cantidad de Paquetes vendidos</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </Table>
+
+            </section>
+        );
     };
 
-    const GraficaPorPax = () => {
-        const ventas = useSelector((state) => state.ventasReducer);
-      
-        const destinosPorPax = {};
-      
-        ventas.forEach((venta) => {
-          const paquete = venta.id_paquete[1];
-      
-          destinosPorPax[paquete] = !destinosPorPax[paquete]
-            ? 1
-            : destinosPorPax[paquete] + 1;
-        });
-      
-        const data = {
-          labels: Object.keys(destinosPorPax),
-          datasets: [
-            {
-              label: 'Cantidad de Pasajeros por Destino',
-              data: Object.values(destinosPorPax),
-      
-              borderWidth: 1,
-            },
-          ],
-        };
+    // const GraficaPorPax = () => {
+    //     const ventas = useSelector((state) => state.ventasReducer);
 
-    };
+    //     const destinosPorPax = {};
+
+    //     ventas.forEach((venta) => {
+    //         const paquete = venta.id_paquete[1];
+
+    //         destinosPorPax[paquete] = !destinosPorPax[paquete]
+    //             ? 1
+    //             : destinosPorPax[paquete] + 1;
+    //     });
+
+    //     const data = {
+    //         labels: Object.keys(destinosPorPax),
+    //         datasets: [
+    //             {
+    //                 label: 'Cantidad de Pasajeros por Destino',
+    //                 data: Object.values(destinosPorPax),
+
+    //                 borderWidth: 1,
+    //             },
+    //         ],
+    //     };
+
+    //     return (
+    //         <div>
+    //             <h2>Gráfico cantidad de Viajeros por Destino</h2>
+    //             <Bar data={data} />
+    //         </div>
+    //     );
+    // };
 
     // ORDENANDO CODIGO HACIA ABAJO
 
     const ListarPaquetes = () => {
         const destinos = useSelector((state) => state.paquetesReducer)
-    
-    return (
-    <section>
-     <h2>Listado de Paquetes</h2>
+
+        return (
+            <section>
+                <h2>Listado de Paquetes</h2>
 
                 {destinos.map((i) => (
-                <Card style={{ width: '18rem' }} className="card">
-                    <Card.Img variant="top" src="holder.js/100px180" />
-    
-                    <Card.Body>
-                        <Card.Title>{i.nombre}</Card.Title>
-                        <Card.Text>{i.precio_mayor}</Card.Text>
-                        <Card.Text>{i.precio_menor}</Card.Text>
-                    </Card.Body>
-                </Card>
+                    <Card style={{ width: '18rem' }} className="card">
+                        <Card.Img variant="top" src="holder.js/100px180" />
+
+                        <Card.Body>
+                            <Card.Title>{i.nombre}</Card.Title>
+                            <Card.Text>{i.precio_mayor}</Card.Text>
+                            <Card.Text>{i.precio_menor}</Card.Text>
+                        </Card.Body>
+                    </Card>
                 ))};
-    </section>
-    );
+            </section>
+        );
     };
 
 
 
     return (<div className="dashboard">
 
-        
-
         <section>
-           
-            
             {/*             
             {ventas.map((venta) =>
 
@@ -140,40 +175,9 @@ const Dashboard = () => {
 
         </section>
 
-        {/* 3.3 */}
-        <section>
-            <h2>Cantidad de Ventas</h2>
-
-            <Table hover size="sm">
-                <thead>
-                    <tr>
-                        <th>Id Paquete</th>
-                        <th>Cantidad de Paquetes vendidos</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </Table>
-
-        </section>
-<ListarPaquetes/>
-<Formulario/>
+        <ListarPaquetes />
+        <Formulario />
+        <cargarVentas></cargarVentas>
         {/* 3.4 */}
         <section>
             <h2>Personas por Destino</h2>
@@ -203,11 +207,8 @@ const Dashboard = () => {
                     </tr>
                 </tbody>
             </Table>
-            <h2>Gráfico cantidad de Viajeros por Destino</h2>
-            {/* <Bar data={data} /> */}
-            {/* no encuentra data porque no esta definido en el return, por eso no debe mostrar los paquetes */}
         </section>
-
+        {/* <GraficaPorPax></GraficaPorPax> */}
         <section>
             <h2>Gráfica de Precios por Destinos</h2>
         </section>
